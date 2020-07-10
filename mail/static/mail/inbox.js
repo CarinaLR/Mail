7c32fc55dae1aff50fe89d95b834763e5cf7550c
CarinaLR <carinalopez@Carinas-MacBook-Pro.local>
2020-07-10 18:03:54 -0400

    500 error fixed

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -5,11 +5,29 @@ document.addEventListener("DOMContentLoaded", function () {
     .addEventListener("click", () => load_mailbox("inbox"));
   document
     .querySelector("#sent")
-    .addEventListener("click", () => load_mailbox("sent"));
+    .addEventListener("click", () => load_mailbox("sent"), loadSent());
   document
     .querySelector("#archived")
     .addEventListener("click", () => load_mailbox("archive"));
   document.querySelector("#compose").addEventListener("click", compose_email);
+
+  //Post request for input.
+  document.querySelector("#compose-form").onsubmit = () => {
+    fetch("/emails", {
+      method: "POST",
+      body: JSON.stringify({
+        recipients: document.querySelector("#compose-recipients").value,
+        subject: document.querySelector("#compose-subject").value,
+        body: document.querySelector("#compose-body").value,
+      }),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        // Print result
+        console.log(result);
+      });
+    return false;
+  };
 
   // By default, load the inbox
   load_mailbox("inbox");
@@ -19,10 +37,6 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector("#emails-view").style.display = "none";
   document.querySelector("#compose-view").style.display = "block";
-
-  document.querySelector("compose-form").onsubmit = () => {
-    postEmail();
-  };
 
   // Clear out composition fields
   document.querySelector("#compose-recipients").value = "";
@@ -39,19 +53,10 @@ function load_mailbox(mailbox) {
   document.querySelector("#emails-view").innerHTML = `<h3>${
     mailbox.charAt(0).toUpperCase() + mailbox.slice(1)
   }</h3>`;
-
-  if (mailbox === "inbox") {
-    loadInbox();
-  }
-  if (mailbox === "sent") {
-    loadSent();
-  }
-  if (mailbox === "archive") {
-    loadArchive();
-  }
 }
 
-function loadInbox() {
+// Load sent emails
+function loadSent() {
   fetch("/emails/inbox")
     .then((response) => response.json())
     .then((emails) => {
@@ -59,68 +64,17 @@ function loadInbox() {
       console.log(emails);
 
       // ... do something else with emails ...
-      emails.forEach(add_email);
+      emails.posts.forEach(add_sent);
     });
 }
 
-// Add a new post with given contents to DOM
-function add_email(contents) {
-  var recipients = document.querySelector("#compose-recipients").value;
-  var subject = document.querySelector("#compose-subject").value;
-  var body = document.querySelector("#compose-body").value;
-
-  contents = {
-    recipients: recipients,
-    subject: subject,
-    body: body,
-  };
-  // Create new email
-  const email = document.createElement("div");
-  email.className = "post";
-  email.innerHTML = contents;
+// Add a new sent email to the sent inbox with given contents to DOM
+function add_sent(contents) {
+  // Create new post
+  const emailSent = document.createElement("div");
+  emailSent.className = "post";
+  emailSent.innerHTML = contents;
 
   // Add post to DOM
-  document.querySelector("#posts").append(email);
-}
-
-function loadSent() {
-  fetch("/emails/sent")
-    .then((response) => response.json())
-    .then((emails) => {
-      // Print emails
-      console.log(emails);
-
-      // ... do something else with emails ...
-    });
-}
-
-function loadArchive() {
-  fetch("/emails/archive")
-    .then((response) => response.json())
-    .then((emails) => {
-      // Print emails
-      console.log(emails);
-
-      // ... do something else with emails ...
-    });
-}
-
-function postEmail() {
-  var recipients = document.querySelector("#compose-recipients").value;
-  var subject = document.querySelector("#compose-subject").value;
-  var body = document.querySelector("#compose-body").value;
-
-  fetch("/emails", {
-    method: "POST",
-    body: JSON.stringify({
-      recipients: recipients,
-      subject: subject,
-      body: body,
-    }),
-  })
-    .then((response) => response.json())
-    .then((result) => {
-      // Print result
-      console.log(result);
-    });
+  document.querySelector("#posts").append(emailSent);
 }
